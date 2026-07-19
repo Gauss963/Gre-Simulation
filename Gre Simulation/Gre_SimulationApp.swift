58f67896f7838fa043a329fda2eb8df040d53f0a
Gauss963 <B09501028@ntu.edu.tw>
2026-07-19 16:29:15 +0800

    Initial Commit

---
+++ Gre Simulation/Gre_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gre_SimulationApp.swift
+//  Gre Simulation
+//
+//  Created by Gauss on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Gre_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
